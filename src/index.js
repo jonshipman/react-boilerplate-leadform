9420f374e1c2b4d7eb215b24450593f1cbec263e
Jon Shipman <jon@jonshipman.com>
2020-08-31 06:52:28 -0500

    fix export warning

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import FormGroup from "./FormGroup";
 import Recaptcha from "./Recaptcha";
 import useForm from "./useForm";
 
-export { Base, Button, FormError, FormGroup, Recaptcha, useForm };
+LeadForm.Base = Base;
+LeadForm.Button = Button;
+LeadForm.FormError = FormError;
+LeadForm.FormGroup = FormGroup;
+LeadForm.Recaptcha = Recaptcha;
+LeadForm.useForm = useForm;
 
 export default LeadForm;
