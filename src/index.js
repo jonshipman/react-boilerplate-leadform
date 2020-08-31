ff7b7531a80f879275a899585fe86e565e0b7639
Jon Shipman <jon@jonshipman.com>
2020-08-31 06:33:36 -0500

    added more exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import LeadForm from "./LeadForm";
+import Base from "./Base";
+import Button from "./Button";
+import FormError from "./FormError";
+import FormGroup from "./FormGroup";
+import Recaptcha from "./Recaptcha";
+import useForm from "./useForm";
+
+export { Base, Button, FormError, FormGroup, Recaptcha, useForm };
 
 export default LeadForm;
