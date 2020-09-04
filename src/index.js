f924cb5115e8f6616545c3f72d7fe4cb2eb7341e
Jon Shipman <jon@jonshipman.com>
2020-09-03 21:03:34 -0500

    add base export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import LeadForm from "./LeadForm";
-import Base from "./Base";
 import Button from "./Button";
 import FormError from "./FormError";
 import FormGroup from "./FormGroup";
 import Recaptcha from "./Recaptcha";
 import useForm from "./useForm";
 
+export * from "./Base";
 export * from "./functions";
 
 export { Base, Button, FormError, FormGroup, Recaptcha, useForm, LeadForm };
