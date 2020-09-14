0dcec58d5b62df6344bf97eedba14ca9762956c6
Jon Shipman <jon@jonshipman.com>
2020-09-14 10:36:19 -0500

    tests no longer run without react-scripts

--- src/index.test.js
+++
@@ -1,9 +0,0 @@
-import * as tests from ".";
-
-Object.keys(tests).forEach((key) => {
-  describe(key, () => {
-    it("is truthy", () => {
-      expect(tests[key]).toBeTruthy();
-    });
-  });
-});
